33adf2db99a1687d5d9587fedc90d3fae62a0c0a
Nicolas Vidal <nico.vidal@duocuc.cl>
2022-09-25 16:31:12 -0300

    async

---
+++ src/bases/11-async-await.js
@@ -0,0 +1,48 @@
+/* const getImagenPromesa =()=>{
+    const promesa=new Promise((resolve,reject)=>{
+        resolve('https://akjsdakjskdja.com')
+    })
+
+    return promesa;
+}
+ */
+
+/* getImagenPromesa().then(console.log) */
+
+//async es para simplificar una funcion a que retone en una promesa
+//para trabajar con un await debe ser trabajado dentro de una funcion async
+const getImagen=async()=>{
+    
+
+    try {
+        const apiKey='SYKq4Etj4sLkKXTPHaS80JvuV1pS66uF';
+        const resp=await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
+    
+        const {data}=await resp.json();
+    
+        const {url}=data.images.original;
+    
+        const img=document.createElement('img');
+    
+        img.src=url;
+        document.body.append(img);
+    
+        console.log(data)
+    } catch (error) {
+
+        console.error(error)
+        
+    }
+
+
+}
+
+getImagen();
+
+
+
+
+
+
+
+
